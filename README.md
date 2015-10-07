ffdebdf62794cce2788fc9f82cc89ad494eb69dd
István Bozsó <bozsoistvan93@gmail.com>
2015-10-07 17:15:34 +0200

    run_rk78.m now reads propagation prameters from file, added comments too

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # orb_prop
 
-Testing repository
+Prameter file should look like this:
+t0      t0 time
+step    # of seconds in a section
+day     # of days to propagate
+name    name of output files
+inital conditions x, y, z, vx, vy, vz separated by spaces
--- run_rk78.m
+++ run_rk78.m
@@ -1,48 +1,77 @@
+% Necessary constants
 GRAVITY_CONST = 6.67384e-11;
 EARTH_MASS = 5.97237e24;
 
-addpath('/home/istvan/orb_prop_matsource/');
+% Acces to function scripts
+addpath ('/home/istvan/orb_prop_matsource/rk_orbit/functions');
 
-%r_0 = [4e6; 5.74456e6; 0; -4571.43; 6565.214; 0]';
-%r_0 = [3.5e6; ; 3.5e6; 4949770; 0; 8e3; 0]';
-r_0 = [7e6; 0; 0; 0; 8e3; 0]';
+% File that contains propagation parameters and initial conditions
+fin = fopen ('ics.dat');
 
-plotname = sprintf('output/v_%.2f_%.2f_%.2f', r_0(4), r_0(5), r_0(6));
+% Processing parameters and initial conditions
+for iii = 1:4
+	str = fgetl(fin);
+	switch ( str(1:findstr(str, ' ') - 1)  )
+		case 't0'
+			t0 = str2num (str(rindex(str, ' '):end))
+		case 'step'
+			step = str2num (str(rindex(str, ' '):end))
+		case 'day'
+			day = str2num (str(rindex(str, ' '):end))
+		case 'name'
+			name = str(rindex(str, ' '):end)
+		otherwise
+			disp('Warning: Unrecognized option.')
+	end
+end
+
+str = fgetl(fin);
+rv_0 = strread(str);
+
+fclose(fin);
+
+plotname = ['output/', name];
 
 vopt = odeset ('RelTol', 1e-4, 'AbsTol', 1, 'NormControl', 'on', 'InitialStep', 125, 'MaxStep', 200);
 
-[a, e, inclination, omega, sinw, cosw, n, tau] = OrbitalElementSet(r_0, 0.0, GRAVITY_CONST * EARTH_MASS);
+% Calculation  of orbital element set from initial condtions
+[a, eccentricity, inclination, omega, sinw, cosw, n, tau] = OrbitalElementSet (rv_0, t0, GRAVITY_CONST * EARTH_MASS);
 
+% Calculating Rot matrix that transforms coordinates from the orbital plane to the 
+% reference plane.
 Rot = CalcRotMtx (sinw, cosw, inclination, omega);
 
-step = 100;
-
-day = 0.1;
-
+% How many 'step' seconds long sections we need
 cicles = ceil((86400 * day ) / step);
 
-timexyzv = rk78_orb_prop (r_0, step, cicles, vopt);
+% Propagation with ode78
+timexyzv = rk78_orb_prop (rv_0, step, cicles, vopt);
 
+% Getting x, y, z coordinates
 xyz = [timexyzv(:,2), timexyzv(:,3), timexyzv(:,4)];
 
+% Getting time points
 time = timexyzv(:, 1);
 
 save('-ascii', [plotname, '.dat'], 'timexyzv');
 
 clear timexyzv;
 
-ellipse = CalcEllipse (time, tau, e, a, n);
+% Calculation of the theoretical coordinates in the orbital plane
+ellipse = CalcEllipse (time, tau, eccentricity, a, n);
 
 ellipse = ellipse * Rot;
 
 save ('-ascii', [plotname, '_ellipse.dat'], 'ellipse');
 
+% Calculating differences between theoretical and numerical coordinates
 delta = ellipse - xyz;
 
 delta = sqrt(sum(delta'.^2));
 
 delta = [time, delta'];
 
+% Plotting orbit
 h=figure(1);
 hold on;
 plot3 (xyz(:, 1) / 1e6, xyz(:, 2) / 1e6, xyz(:, 3) / 1e6, 'ob;RK4;', 'linewidth', 1, ...
@@ -54,12 +83,13 @@ ylabel('y [1000 km]');
 zlabel('z [1000 km]');
 legend ('location', 'northeastoutside');
 print(h,'-dpng','-color', [plotname, '.png']);
-hold off;
+hold off
 
 save('-ascii', [plotname, '_delta.dat'], 'delta');
 
+% Plotting deltas
 h = figure(2);
-clf;
+clf
 hold on
 plot (delta(:,1) / 86400, delta(:,2));
 print(h,'-dpng','-color', [plotname, '_delta' , '.png']);
